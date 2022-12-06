129d69e4094836f38877a19c51dddef95c808ab6
Adrian Bailador Panero <adrianbailador@hotmail.com>
2022-12-06 23:30:53 +0000

    Agregar archivos de proyecto.

---
+++ ASPTest/Models/Beer.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPTest.Models
+{
+    public class Beer
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Brand { get; set; }
+
+    }
+}
---
+++ ASPTest/Services/IBeer.cs
@@ -0,0 +1,11 @@
+﻿using ASPTest.Models;
+
+namespace ASPTest.Services
+{
+    public interface IBeer
+    {
+        public IEnumerable<Beer> Get(); //Nos devuelva toda la información de cerveza
+
+        public Beer? Get(int id);
+    }
+}
